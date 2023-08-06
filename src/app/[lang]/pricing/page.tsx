389d0604b0f5b12cff4fe28a45d48c288890a11f
Kevin Chung <kevin@bluextrade.com>
2023-08-06 12:55:14 +0800

    add language header

--- src/app/[lang]/pricing/page.tsx
+++ src/app/[lang]/pricing/page.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 
 export default function Page() {
-  return <>
-    <h1>Hello, Pricing Page!</h1>
-    <Link href="/pricing/detail">Pricing Detail</Link>
-  </>
+  return (
+    <div className="relative w-full h-fit overflow-x-hidden">
+      <div className="none lg:block">
+        <div className="absolute w-full h-[31.875rem] bg-secondary"
+        >
+          <div className="absolute w-0 h-0 block border-l-[100vw] border-l-transparent border-b-[8rem] border-b-white border-solid left-0 bottom-0"></div>
+        </div>
+      </div>
+      <h1>Hello, Pricing Page!</h1>
+      <Link href="/pricing/detail">Pricing Detail</Link>
+    </div>
+  );
 }
